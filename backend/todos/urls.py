2013b6f789e32d3867d9c4a92694e5b9a2d2388f
Paweł Sacawa <pawelsacawa@gmail.com>
2021-06-18 23:27:15 -0400

    Add: frontend, backend dockerfiles with buildkit caching

--- backend/todos/urls.py
+++ backend/todos/urls.py
@@ -2,9 +2,14 @@ from django.urls import re_path, include, path
 
 from rest_framework.routers import SimpleRouter
 
+class MySimpleRouter(SimpleRouter): 
+    def __init__(self):
+        self.trailing_slash ="/?"
+        super(SimpleRouter, self).__init__()
+
 from .views import TodoViewSet, echo
 
-router = SimpleRouter()
+router = MySimpleRouter()
 router.register("todos", TodoViewSet, basename="todos")
 
 
